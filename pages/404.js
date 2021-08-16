a31bd3d013ff1045e478ab10a80b906d50469048
Chethan Poojary <chethan.poojary@niveussolutions.com>
2021-08-16 15:47:14 +0530

    added more css

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,62 @@
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
-  }
+  return (
+    <>
+     <section className="page_404">
+        <div className="container">
+          <div className="row">	
+            <div className="col-sm-12 ">
+              <div className="col-sm-10 col-sm-offset-1  text-center">
+                <div className="four_zero_four_bg">
+                  <h1 className="text-center ">404</h1>
+                </div>
+                <div className="contant_box_404">
+                  <h3 className="h2">
+                    Look like you're lost
+                  </h3>
+                  <p>the page you are looking for not avaible!</p>
+                  <a href className="link_404">Go to Home</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <style jsx>{`
+  
+
+.page_404{ padding:40px 0; background:#fff; font-family: 'Arvo', serif;    max-width: 36rem;
+padding: 0 1rem;
+margin: 3rem auto 6rem;
+
+}
+
+.page_404  img{ width:100%;}
+
+.four_zero_four_bg{
+
+background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+height: 400px;
+background-position: center;
+}
+
+
+.four_zero_four_bg h1{
+font-size:80px;
+}
+
+.four_zero_four_bg h3{
+   font-size:80px;
+   }
+   
+   .link_404{			 
+color: #fff!important;
+padding: 10px 20px;
+background: #39ac31;
+margin: 20px 0;
+display: inline-block;}
+.contant_box_404{ margin-top:-50px;}
+`}</style>
+    </>
+    
+  )
+}
